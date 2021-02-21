e879e289440000e3a0d1dfd4170311736f7a4043
Adrien <>
2021-02-21 20:39:40 +0100

    Trajectory minimization added

--- scripts/trajectory_clustering.py
+++ scripts/trajectory_clustering.py
@@ -1,5 +1,4 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import csv
 
 np.random.seed()
@@ -15,15 +14,15 @@ class Trajectories:
     def addRandomTrajectory(self, maxValue=10, nbPoints=10, nbCoordinates=2):
         self.trajectories.append(maxValue * np.random.random((nbPoints, nbCoordinates)))
 
-    def addTrajectoryFromCsv(self, dir):
-        with open(dir) as csv_file:
+    def addTrajectoryFromCsv(self, file):
+        with open(file) as csv_file:
             csv_reader = csv.reader(csv_file, delimiter = ',')
             line_count = 0
             t = []
             x = y = z = -1
             for row in csv_reader:
                 if line_count == 0:
-                    print(f'Column names are {", ".join(row)}')
+                    print(f"Column names are {', '.join(row)}")
                     # Automatic version
                     for i in range(len(row)):
                         if row[i] == "CameraPosition.x":
@@ -32,7 +31,7 @@ class Trajectories:
                             y = i
                         elif row[i] == "CameraPosition.z":
                             z = i
-                    print(f'before : x = {x}, y = {y}, z = {z}')
+                    print(f"before : x = {x}, y = {y}, z = {z}")
                     # If the col isn't found
                     if x == -1:
                         x = int(input("Camera position X not found. Enter the row (index - 1) : "))
@@ -40,33 +39,62 @@ class Trajectories:
                         y = int(input("Camera position Y not found. Enter the row (index - 1) : "))
                     if z == -1:
                         z = int(input("Camera position Z not found. Enter the row (index - 1) : "))
-                    print(f'after : x = {x}, y = {y}, z = {z}')
+                    print(f"after : x = {x}, y = {y}, z = {z}")
                 else:
                     t.append([float(row[x]), float(row[y]), float(row[z])])
                 line_count += 1
             self.addTrajectory(t)
-            print(f'Processed {line_count} lines.')
-    
+            print(f"Processed {line_count} lines.")
+
+
     def getTrajectory(self, id):
         return self.trajectories[id]
 
+    # Make all trajectories the same length
+    def attuneTrajectories(self):
+
+        def minimizedTrajectory(trajectory):
+            minimized = []
+
+            # init last_point & last_direction
+            nb_dimensions = trajectory.shape[1]
+            last_point = last_direction = np.zeros(nb_dimensions)
+
+            def updateDirection(p1, p2):
+                direction = np.zeros(nb_dimensions)
+                for i in range(nb_dimensions):
+                    direction[i] = 1 if (p1[i] > p2[i]) else -1
+                return direction
+
+            for i in range(len(trajectory)):
+                point = trajectory[i]
+                new_direction = updateDirection(point, last_point)
+                if i > 0 and not (last_direction == new_direction).all():
+                    minimized.append(last_point)
+                last_direction = new_direction
+                last_point = point
+            minimized.append(trajectory[len(trajectory) - 1])
+            return np.array(minimized)
+        for i in range(len(self.trajectories)):
+            self.trajectories[i] = minimizedTrajectory(self.trajectories[i])
+
     def printTrajectories(self):
-        for trajectory in self.trajectories:
-            print(trajectory)
+        print("-- Trajectories --")
+        for i in range(len(self.trajectories)):
+            print(f"Trajectory #{i} :\n{self.trajectories[i]}")
 
     def showTrajectories(self):
         import matplotlib.pyplot as plt
-        tr = np.asarray(self.trajectories)
-        size = tr.shape[2]
-        # 2D visualisation
-        if size == 2:
+        nb_dimensions = self.trajectories[0].shape[1]
+        # 2D visualization
+        if nb_dimensions == 2:
             for trajectory in self.trajectories:
                 plt.plot(trajectory[:, 0], trajectory[:, 1]) #x, y
-        # 3D visualisation
-        elif size == 3:
+        # 3D visualization
+        elif nb_dimensions == 3:
             from mpl_toolkits.mplot3d import Axes3D
             fig = plt.figure()
-            ax = fig.gca(projection='3d')
+            ax = fig.gca(projection="3d")
             for trajectory in self.trajectories:
                 ax.plot(trajectory[:, 0], trajectory[:, 1], trajectory[:, 2])
 
@@ -74,6 +102,10 @@ class Trajectories:
         plt.ylabel("y")
         plt.show()
 
+    def completeDisplay(self):
+        self.getTrajectoriesDistances(verbose=True)
+        self.printTrajectories()
+        self.showTrajectories()
 
     ## Different heuristics to compute distance between two trajectories
 
@@ -93,7 +125,7 @@ class Trajectories:
             distance += distanceBetweenTwoPoints(t1[i], origin_difference + t2[i]) ** 2
         distance = np.sqrt(distance / size)
         if (verbose):
-            print("Distance between trajectory {} & {} : {}".format(index1, index2, distance))
+            print(f"Distance between trajectory {index1} & {index2} : {distance}")
         return distance
 
     # Adds a translation to heuristic0 by pasting the 2nd trajectory origin onto the 1st
@@ -119,31 +151,47 @@ class Trajectories:
         return distances
 
 
-## Create similar trajectories
-similar_trajectories = Trajectories()
+## Different kind of Trajectories call
 
-t0 = [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9]]
-t1 = [[0, 4], [1, 5], [2, 6], [3, 7], [4, 8], [5, 9], [6, 10], [7, 11], [8, 12], [9, 13]]
-similar_trajectories.addTrajectory(t0)
-similar_trajectories.addTrajectory(t1)
+# Create two same trajectories but spaced out by 4 units
+def createSimilarTrajectories(minimize=False):
+    similar_trajectories = Trajectories()
 
-#similar_trajectories.distanceBetweenTwoTrajectories(0, 1, verbose=True)
-similar_trajectories.showTrajectories()
+    t0 = [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9]]
+    t1 = [[0, 4], [1, 5], [2, 6], [3, 7], [4, 8], [5, 9], [6, 10], [7, 11], [8, 12], [9, 13]]
+    similar_trajectories.addTrajectory(t0)
+    similar_trajectories.addTrajectory(t1)
+    similar_trajectories.attuneTrajectories()
 
-print() # \n
+    similar_trajectories.completeDisplay()
+    if (minimize):
+        similar_trajectories.attuneTrajectories()
+        similar_trajectories.completeDisplay()
+    return similar_trajectories
 
-## Create random trajectories
-random_trajectories = Trajectories()
-random_trajectories.addTrajectory(t0)
-nb_trajectories = 10
-for i in range(nb_trajectories - 1):
-    random_trajectories.addRandomTrajectory()
+# Create nb_trajectories random trajectories
+def createRandomTrajectories(nb_trajectories=10, minimize=False):
+    random_trajectories = Trajectories()
 
-#distances = random_trajectories.getTrajectoriesDistances(verbose=True)
-#print(distances)
-#random_trajectories.printTrajectories()
-#random_trajectories.showTrajectories()
+    t0 = [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 5], [6, 6], [7, 7], [8, 8], [9, 9]]
+    random_trajectories.addTrajectory(t0)
+    for i in range(nb_trajectories - 1):
+        random_trajectories.addRandomTrajectory()
 
-csv_traj = Trajectories()
-csv_traj.addTrajectoryFromCsv("datapoints/participant7trial2-ontask-100.csv")
-csv_traj.showTrajectories()
+    random_trajectories.completeDisplay()
+    if (minimize):
+        random_trajectories.attuneTrajectories()
+        random_trajectories.completeDisplay()
+    return random_trajectories
+
+# Create one trajectory from a file
+def createCSVTrajectories(file):
+    csv_trajectories = Trajectories()
+
+    csv_trajectories.addTrajectoryFromCsv(file)
+    csv_trajectories.showTrajectories()
+    return csv_trajectories
+
+#createSimilarTrajectories(minimize=True)
+#createRandomTrajectories(nb_trajectories=2, minimize=True)
+createCSVTrajectories("../datapoints/participant7trial2-ontask-100.csv")
