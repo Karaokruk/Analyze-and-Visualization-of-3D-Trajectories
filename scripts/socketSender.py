b963438d88a82f48f96eb8a509d27356d6592cc2
Antonin <antonin33290@gmail.com>
2021-03-15 14:27:47 +0100

    Enhancing socket sender

--- scripts/socketSender.py
+++ scripts/socketSender.py
@@ -3,12 +3,20 @@ from trajectory_clustering import createCSVTrajectories
 from k_mean import kmean
 
 import socket
+import sys
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-print("Connecting...")
-s.connect(("192.168.1.36", 5000))
+#hostname = socket.gethostname()
+#ip_address = socket.gethostbyname(hostname)
+ip_address = sys.argv[1]
+port = int(sys.argv[2])
+print(f"Connecting to {ip_address}, port {port}")
+s.connect((ip_address, port))
 
-traj = createCSVTrajectories("../datapoints/SmallMultipleVR_Study/Study 2/Participant_7_HeadPositionLog.csv", verbose = False)
+trajDir = s.recv(9999999)
+s.send("Python : directory file received".encode())
+
+traj = createCSVTrajectories(trajDir.decode("utf8"), verbose = False)
 a = kmean(traj, k = 3, method = 1, verbose = False)
 
 s.send(str(a).encode())
