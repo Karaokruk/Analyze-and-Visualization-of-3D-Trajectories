1e98acd117e41a003bfb3b464204c9aa1f91dbe6
Antonin <antonin33290@gmail.com>
2021-03-15 14:59:06 +0100

    Enabling multiple file loading

--- scripts/socketSender.py
+++ scripts/socketSender.py
@@ -13,10 +13,24 @@ port = int(sys.argv[2])
 print(f"Connecting to {ip_address}, port {port}")
 s.connect((ip_address, port))
 
-trajDir = s.recv(9999999)
-s.send("Python : directory file received".encode())
+trajLen = s.recv(9999999)
+trajLen = int(trajLen.decode("utf8"))
+message = "Python : counting " + str(trajLen) + " files"
+s.send(message.encode())
 
-traj = createCSVTrajectories(trajDir.decode("utf8"), verbose = False)
+traj = Trajectories()
+for i in range(trajLen):
+    trajDir = s.recv(9999999)
+    trajDir = trajDir.decode("utf8")
+
+    traj.addTrajectoriesFromCsv(trajDir)
+
+    message = "Python : loaded file " + str(trajDir)
+    s.send(message.encode())
+    print(message)
+
+
+traj.attuneTrajectories(0.98, 0)
 a = kmean(traj, k = 3, method = 1, verbose = False)
 
 s.send(str(a).encode())
